5215bb0f66b37d2b5278d5976b08875008bc4b3f
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-08-27 17:53:50 +0900

    いろいろ追加

---
+++ Assets/Scripts/Example01.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example01 : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(DelayLog());
+    }
+
+    IEnumerator DelayLog()
+    {
+        Debug.Log("5秒後にログが表示されます");
+        yield return new WaitForSeconds(5.0f);
+        Debug.Log("Here!");
+    }
+}
---
+++ Assets/Scripts/Example02.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example02 : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(CallParent());
+    }
+
+    IEnumerator CallParent()
+    {
+        Debug.Log("CallParentが呼ばれました");
+        yield return StartCoroutine(CallChild());
+        Debug.Log("CallParentが終了します");
+    }
+
+    IEnumerator CallChild()
+    {
+        Debug.Log("CallChildが呼ばれました");
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log("CallChildが終了します");
+    }
+}
