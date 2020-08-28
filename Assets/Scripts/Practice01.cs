93a143bdeba05aebb73a7313d477b12d65a0f082
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-08-28 14:36:29 +0900

    練習問題とか追加

---
+++ Assets/Scripts/Practice01.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice01 : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(CallA());
+    }
+
+    IEnumerator CallA()
+    {
+        Debug.Log("CallAが呼ばれました");
+        yield break;
+        Debug.Log("CallAが終了しました");
+    }
+}
---
+++ Assets/Scripts/Practice02.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice02 : MonoBehaviour
+{
+    private bool m_bIsClicked;
+    void Start()
+    {
+        StartCoroutine(Base());
+    }
+    public void ButtonClicked()
+    {
+        m_bIsClicked = true;
+    }
+    IEnumerator Base()
+    {
+        m_bIsClicked = false;
+		#region 回答欄
+
+		#endregion
+		Goal();
+        yield break;
+    }
+    void Goal()
+    {
+        Debug.Log("Goal");
+    }
+}
